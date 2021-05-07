78a4a79bcd46b1240c74df4b7aea912b7e050754
Aaron S Grincewicz <veganimus@icloud.com>
2021-05-06 21:12:08 -0400

    Prototype Battle Arena and characters.

---
+++ Assets/GameDevHQ/FileBase/3D/Characters/Robots/Mech_02/Script/Parent_Turret_Y.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parent_Turret_Y : MonoBehaviour
+{
+
+    public GameObject Parent_Point;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = new Vector3(transform.position.x, Parent_Point.transform.position.y, Parent_Point.transform.position.z);
+    }
+}
