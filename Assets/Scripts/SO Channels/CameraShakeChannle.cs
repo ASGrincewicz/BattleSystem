0f0b716868c3970fa19d7c2c0bb36bed5a962494
Aaron S Grincewicz <veganimus@icloud.com>
2021-06-02 20:19:23 -0400

    Added rotation classes and camera shake.

---
+++ Assets/Scripts/SO Channels/CameraShakeChannle.cs
@@ -0,0 +1,18 @@
+﻿// Aaron Grincewicz Veganimus@icloud.com 6/2/2021
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Veganimus
+{
+    [CreateAssetMenu(menuName ="Camera Shake Channel")]
+    public class CameraShakeChannel : ScriptableObject
+    {
+        public UnityEvent OnCameraShake;
+
+        public void RaiseCameraShakeEvent()
+        {
+            if (OnCameraShake != null)
+                OnCameraShake.Invoke();
+        }
+    }
+}
---
+++ Assets/Scripts/Utils/CameraShake.cs
@@ -0,0 +1,36 @@
+﻿// Aaron Grincewicz ASGrincewicz@icloud.com 6/2/2021
+using System.Collections;
+using UnityEngine;
+
+namespace Veganimus
+{
+    public class CameraShake : MonoBehaviour
+    {
+       [SerializeField] private bool _isShaking;
+       [SerializeField] private float _duration;
+       [SerializeField] private float _magnitude;
+       [SerializeField] private CameraShakeChannel _cameraShakeChannel;
+
+        private void OnEnable() => _cameraShakeChannel.OnCameraShake.AddListener(StartCameraShake);
+        private void OnDisable() => _cameraShakeChannel.OnCameraShake.RemoveListener(StartCameraShake);
+        private void StartCameraShake() => StartCoroutine(CameraShakeRoutine());
+
+        private IEnumerator CameraShakeRoutine()
+        {
+            var defaultPosition = transform.position;
+            var elapsedTime = 0f;
+
+            while(elapsedTime < _duration)
+            {
+                _isShaking = true;
+                float x = Random.Range(-1, 1) * _magnitude;
+                float y = Random.Range(-1, 1) * _magnitude;
+                transform.position = new Vector3(x, y, -10f);
+                elapsedTime += Time.deltaTime;
+                yield return null;
+            }
+            transform.position = defaultPosition;
+            _isShaking = false;
+        }
+    }
+}
